e1e57e4cde498023f71b5f46c9f2b5370f434a11
Per Samuelsson <per@starcounter.com>
2018-02-02 10:56:19 +0100

    Sample projects and a script to run it.

---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+
+namespace SampleApp {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var dir = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+            var targetPath = Path.Combine(dir, @"..\..\..\..\Target\bin\Debug\NetStandard2.0\Target.dll");
+
+            var module = AssemblyDefinition.ReadAssembly(targetPath).MainModule;
+
+            var @ref = module.GetTypeReferences().First(r => r.Name.Contains("Ninject"));
+            @ref.Resolve();
+        }
+    }
+}
---
+++ src/run.bat
@@ -0,0 +1,4 @@
+@echo off
+
+dotnet build .\Target
+dotnet run -p .\SampleApp\SampleApp.csproj
